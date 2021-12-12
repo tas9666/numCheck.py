c08c9e3d3d048417689728ad6ba6ec211c6aba5d
tas9666 <93866942+tas9666@users.noreply.github.com>
2021-12-12 17:05:54 +0400

    Add files via upload

---
+++ app/000.py
@@ -0,0 +1,15 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import math 
+
+print(" Program to demonstrate the use of Pyton library-") 
+
+num=121.121 
+
+print(math.sqrt(num)) 
+
+print(math.ceil(num)) 
+
+print(math.floor(num)) 
+
+ 
